6f7f0e175afe8e68d497cc683115cc7d2d38b991
Gem George <gemgeorge4@gmail.com>
2017-10-04 12:33:26 +0530

    Update iball-utstar.py

--- iball-utstar.py
+++ iball-utstar.py
@@ -21,12 +21,12 @@ def get_info(url):
 	else:
 		if "ADSL2+" in res:		
 			print colored('[INF]','green'), 'Device identified: iBall ADSL2+ Home Router WRA150N'
-			print colored('[RES]','red'), 'Vulnerble to CVE-2017-14244'
+			print colored('[RES]','red'), 'Vulnerable to CVE-2017-14244'
 			print colored('[RES]','red'),'Firmware Version: FW' + find_between(res, 'FW', '</td>')
 		else:
 			if "96338W" in res:	
 				print colored('[INF]','green'), 'Device identified: UTStar WA3002G4 ADSL Broadband Modem'
-				print colored('[RES]','red'), 'Vulnerble to CVE-2017-14243'
+				print colored('[RES]','red'), 'Vulnerable to CVE-2017-14243'
 				get_cred(url)
 			else:
 				print colored('[INF]','green'), 'Device not vulnerable to CVE-2017-6558, CVE-2017-14243 or CVE-2017-14244'
@@ -47,11 +47,11 @@ def find_between( s, first, last ):
         	return ""
 
 def display_info():
-	print colored('\r\n¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦','green')
-	print colored('¦','green'), '  Check for CVE-2017-6558, CVE-2017-14243 & CVE-2017-14244	', colored('¦','green')
-	print colored('¦','green'), '		 Created by: Gem George				' , colored('¦','green')
-	print colored('¦','green'), '	 Website: https://www.techipick.com/ 			', colored('¦','green')
-	print colored('¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦¦','green')
+	print colored('\r\nÂ¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦','green')
+	print colored('Â¦','green'), '  Check for CVE-2017-6558, CVE-2017-14243 & CVE-2017-14244	', colored('Â¦','green')
+	print colored('Â¦','green'), '		 Created by: Gem George				' , colored('Â¦','green')
+	print colored('Â¦','green'), '	 Website: https://www.techipick.com/ 			', colored('Â¦','green')
+	print colored('Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦Â¦','green')
 	print "\r\n"
 	print colored('[SET]','blue'), 'Target URL: ', sys.argv[1]
 
